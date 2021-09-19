3bda7281f6506f913ce5f78d7f76f5e1c58061d2
EstefanyVergara <vteffa@gmail.com>
2021-09-19 18:05:04 -0300

    página 1 ok, routing

--- src/components/Identify.jsx
+++ src/components/Identify.jsx
@@ -7,16 +7,14 @@ const Identify = () => {
     return (
         <Fragment>
         <div className = "container">
-          <img src = {Logo} alt = "burgir" className = "logo"/>
-          
-          <div className="selectWorker">
-          <h1 className= "text1">SELECCIONA UNA OPCIÓN</h1>
-          <button className = 'btn-primary'> Mesero</button>
-          <button className = 'btn-secondary' disabled> Cocina</button>
-          </div>
-          </div>
-          
-          </Fragment>
+            <img src = {Logo} alt = "burgir" className = "logo"/>
+                <div className="selectWorker">
+                    <h1 className= "text1">SELECCIONA UNA OPCIÓN</h1>
+                    <button className = 'btn-primary'> Mesero</button>
+                    <button className = 'btn-secondary' disabled> Cocina</button>
+                </div>
+        </div>
+        </Fragment>
     );
 
 };
