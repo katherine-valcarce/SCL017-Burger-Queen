25e23834273bb5246f3cdbdcd0246b600f7d5875
Katherine-valcarce <katherine.v@live.cl>
2021-09-20 11:47:22 -0300

    Imagenes de usuarios

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -7,13 +7,53 @@ const User = () => {
     <Fragment>
       <div className="container">
         <div className="row">
+
           <div className="card">
             <img src={camila} alt="camila" />
             <div className="card-text">
               <h3>Camila Zuñiga</h3>
             </div>
           </div>
-        </div>
+
+          <div className="card">
+            <img src={camila} alt="camila" />
+            <div className="card-text">
+              <h3>Carolina Quintanilla</h3>
+            </div>
+          </div>
+          </div>
+
+          <div className="row">
+          <div className="card">
+            <img src={camila} alt="camila" />
+            <div className="card-text">
+              <h3>Eva Muñoz</h3>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src={camila} alt="camila" />
+            <div className="card-text">
+              <h3> Juanita Gonzalez</h3>
+            </div>
+          </div>
+          </div>
+
+          <div className="row">
+          <div className="card">
+            <img src={camila} alt="camila" />
+            <div className="card-text">
+              <h3>Fernanda Rojas</h3>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src={camila} alt="camila" />
+            <div className="card-text">
+              <h3> Cecilia Hernandez</h3>
+            </div>
+          </div>
+          </div>
       </div>
     </Fragment>
   );
