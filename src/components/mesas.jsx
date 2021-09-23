0ea5a457be50ce3de16d105e90dccb33b3f474b8
Katherine-valcarce <katherine.v@live.cl>
2021-09-23 08:45:01 -0300

    menu JSON - agregar ID

--- src/components/mesas.jsx
+++ src/components/mesas.jsx
@@ -4,13 +4,13 @@ import '../components/mesas.css'
 const Mesas = () => {
   return (
 <div className=" Menu-dropdown">
-  <button className=" btnMesa btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+  <button className="btnMesa btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
     ESCOGE MESA 
   </button>
   <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton2">
     <li><a className="dropdown-item" href="#">Mesa 1</a></li>
     <li><a className="dropdown-item" href="#">Mesa 2</a></li>
-    <li><a className="dropdown-item" href="#">Mesa3</a></li>
+    <li><a className="dropdown-item" href="#">Mesa 3</a></li>
   </ul>
 </div>
   )
