e42723950db7fb0012cee245cb71731e8ba202a9
EstefanyVergara <vteffa@gmail.com>
2021-09-23 17:09:51 -0300

    actualizando rama

--- src/components/breakfast.jsx
+++ src/components/breakfast.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Ordersummary from "./orderSummary";
 import "../components/menu.css";
 import {NavBreakfast} from "./Nav";
+import data from './data/menu.json'
+
 
 export const Breakfast = () => {
+
+    const dataBreakfast = data.Breakfast
+    console.log(dataBreakfast);
+    
+
   return (
-    <div className="menuContainer">
+    <div classNameName="menuContainer">
       <Ordersummary />
       <NavBreakfast/>
+     
     </div>
   )
 }
