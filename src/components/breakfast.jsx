c324f6c1c12ec3a5e476f4be764a5c84f3675f45
Katherine-valcarce <katherine.v@live.cl>
2021-09-24 16:16:28 -0300

    card breakfast

--- src/components/breakfast.jsx
+++ src/components/breakfast.jsx
@@ -8,13 +8,27 @@ import data from './data/menu.json'
 export const Breakfast = () => {
 
     const dataBreakfast = data.Breakfast
+    console.log(dataBreakfast)
     
 
   return (
     <div className="menuContainer">
       <Ordersummary />
       <NavBreakfast/>
-      <img src={dataBreakfast} alt="" />
+
+      <div className="breakfastMenu">
+        
+        <div className="card">
+          <img src={dataBreakfast[0].img} className="card-img-top" alt="..."/>
+          <div className ="card-body">
+            <h5 className ="card-title">{dataBreakfast[0].product}</h5>
+            <p className ="card-price">${dataBreakfast[0].price}</p>
+          </div>
+        </div>
+        
+
+      </div>
+
     </div>
   )
 }
