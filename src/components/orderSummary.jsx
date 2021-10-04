32dc006a78a14cc23f5552b6d9ab151af76f8e62
EstefanyVergara <vteffa@gmail.com>
2021-10-04 08:58:45 -0300

    Cambio de rama, todo listo

--- src/components/orderSummary.jsx
+++ src/components/orderSummary.jsx
@@ -13,13 +13,46 @@ return (
     </div>
     <ItemsOrder onAdd={onAdd} cartItems={cartItems} onRemove={onRemove} removeItems={removeItems} />
     <div className='btn-order'>
-    <button className="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal"> ENVIAR </button>
     <button className="btn btn-dark" > CANCELAR </button>
+    <a className="btn btn-light" data-bs-toggle="modal" href="#exampleModalToggle" role="button">ENVIAR</a>
     </div>
     </div>
 
+    <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalToggleLabel">CONFIRMAS EL PEDIDO?</h5>
+      </div>
+      <div class="modal-body">
+        RESUMEN DEL PEDIDO
+      </div>
+      <div class="modal-footer">
+      <button type="button" className="btn btn-dark" data-bs-dismiss="modal">NO</button>
+        <button class="btn btn-light" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">SI</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        SOLICITUD ENVIADA A COCINA
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
 
-<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+
+
+{/* <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
@@ -34,12 +67,13 @@ return (
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-dark" data-bs-dismiss="modal">NO</button>
-        <button type="button" className="btn btn-light"data-bs-toggle="modal" data-bs-target="#exampleModal">SI</button>
+        <button type="button" className="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal">SI</button>
+
       </div>
     </div>
   </div>
-</div>
-    
+</div> */}
+
 </>
 
 
