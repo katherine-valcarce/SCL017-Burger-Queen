124883f3efa37d14031ba1f06039091f6b837e08
EstefanyVergara <vteffa@gmail.com>
2021-10-06 13:06:30 -0300

    Cambios limpiando css

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAG09lDMYwNcQoXc2qHZIP09CV8I0UIXWE",
     authDomain: "burgirspace.firebaseapp.com",
@@ -13,7 +10,7 @@ const firebaseConfig = {
     appId: "1:624692355412:web:3d84081729afa80e98e8c1"
 };
 
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 
 export {firebase};
